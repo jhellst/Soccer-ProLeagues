789cfadb21f3825c2128f46698ad14d07f6d68ed
Joshua <jhellst@gmail.com>
2024-04-04 18:35:58 -0700

    Added xCircle component using FA svg icon.

---
+++ soccer-proleagues-frontend/src/xCircle.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import xMarkCircle from './images/circle-xmark-solid.svg';
+
+function xCircle({ listType, unfollowLeague, unfollowTeam, user_id, id }) {
+
+  const handleSubmitRemove = () => {
+    if (listType === "League") {
+      unfollowLeague(user_id, id)
+    } else {
+      unfollowTeam(user_id, id)
+    }
+  };
+
+return (
+  // <button className='removeButton' onClick={handleSubmitRemove}></button>
+  <xMarkCircle className='removeButton' onClick={handleSubmitRemove}/>
+);
+}
+
+export default xCircle;
